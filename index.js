eeb5b39089cf9c53bda6e61acd805fbdee827dbb
aveygo <aveygo.au@gmail.com>
2024-12-21 10:56:09 +1100

    victoria data

--- index.js
+++ index.js
@@ -17,12 +17,14 @@ async function fetch_json(url) {
   return await report.json()
 }
 
-async function cached_fetch_json(url) {
+async function cached_fetch_json(url, ttl) {
   /*
     Mainly for dev purposes to limit number of weather requests
   */
 
-  let ttl = 60 * 15 * 1000;
+  if (ttl == null) {
+    ttl = 60 * 15 * 1000;
+  }
   let data = localStorage.getItem(url);
 
   // Check if already in cache
@@ -83,27 +85,103 @@ async function cached_contours() {
   return contours.data;
 }
 
+function bounding_box(points) {
+  let min_lat = points[0][0];
+  let min_lon = points[0][1];
+  let max_lat = points[0][0];
+  let max_lon = points[0][1];
+
+  for (let point of points) {
+    if (point[0] < min_lat) min_lat = point[0];
+    if (point[0] > max_lat) max_lat = point[0];
+    if (point[1] < min_lon) min_lon = point[1];
+    if (point[1] > max_lon) max_lon = point[1];
+  }
+
+  return [min_lat, min_lon, max_lat, max_lon]
+}
+
+function bounding_box_size(points) {
+  let bounds = bounding_box(points);
+  let min_lat = bounds[1];
+  let min_lon = bounds[0];
+  let max_lat = bounds[3];
+  let max_lon = bounds[2];
+
+  // Approximation of distances in meters for lat/lon differences
+  const earthRadius = 6371000; // in meters
+  const latDist = ((max_lat - min_lat) * Math.PI / 180) * earthRadius;
+  const avgLat = (min_lat + max_lat) / 2; // Average latitude for accurate longitude scaling
+  const lonDist = ((max_lon - min_lon) * Math.PI / 180) * earthRadius * Math.cos(avgLat * Math.PI / 180);
+
+  return Math.abs((latDist * lonDist) / 10000); // hectares
+}
+
+function get_size(fire) {
+  // Description is mainly only within NSW data, but is the best outcome
+  // cause they set it to 0 when the fire is out 
+  if (fire.properties.description != null) {
+    const match = fire.properties.description.match(/SIZE:\s*(\d+)\s*ha/);
+    if (match) { return parseInt(match[1], 10); }
+  }
+
+  
+  // Sum of bounding boxes, kinda inaccurate as bush fires don't
+  // burn in squares, but it's easy to calculate
+  let sizes = [];
+  if (fire.geometry != null) {
+    if (fire.geometry.type === "Polygon") {
+      sizes.push(bounding_box_size(fire.geometry.coordinates[0]));
+      
+    }
+  }
+  let size = sizes.reduce((a, b) => a + b, 0);
+
+  if (size != null) {return size}
+  return 0
+}
+
+function get_position(fire) {
+
+  // Could be given exact point immediately
+  if (fire.geometry.geometries != null) {
+    let maybe_point = fire.geometry.geometries.at(-1)
+    if (maybe_point.type == "Point") {
+      return maybe_point.coordinates
+    }
+  }
+
+  // Estimate based on first given bounds
+  
+  if (fire.geometry.type == "Polygon") {
+    let bounds = bounding_box(fire.geometry.coordinates[0]);
+
+    let min_lat = bounds[0];
+    let min_lon = bounds[1];
+    let max_lat = bounds[2];
+    let max_lon = bounds[3];
+    
+    return [(max_lat + min_lat)/2, (max_lon + min_lon)/2]
+  }
+  
+  return null
+}
+
 async function handle_fire(fire) {
   /*
     Asynchronously fetch weather data & calculate contours 
   */
   
-  // Hectares may not be present, so we assume size = 0
-  const match = fire.properties.description.match(/SIZE:\s*(\d+)\s*ha/);
-  let hectares = 0;
-  if (match) {
-    hectares = parseInt(match[1], 10);
-  }
-  
-  // Only compute for fires with a "center" position
-  let maybe_point = fire.geometry.geometries.at(-1)
-  if (maybe_point.type == "Point" && hectares > 0) {
+  let hectares = get_size(fire);
+  let center = get_position(fire);
+
+  if ((center!=null) && hectares > 0) {
 
     // Assumed as center of fire
-    let lat = maybe_point.coordinates[1]
-    let lon = maybe_point.coordinates[0]
+    let lat = center[1]
+    let lon = center[0]
 
-    let weather = await cached_fetch_json("https://api.open-meteo.com/v1/forecast?latitude=" + lat + "&longitude=" + lon + "&hourly=temperature_2m,wind_speed_10m,wind_speed_120m,wind_direction_10m,wind_direction_120m&forecast_days=1")
+    let weather = await cached_fetch_json("https://api.open-meteo.com/v1/forecast?latitude=" + lat + "&longitude=" + lon + "&hourly=temperature_2m,wind_speed_10m,wind_speed_120m,wind_direction_10m,wind_direction_120m&forecast_days=1", 6 * 60 * 60 * 1000)
 
     // Get current hour as weather returns hourly data
     const d = new Date();
@@ -127,13 +205,16 @@ async function handle_fire(fire) {
 
 async function build_contours() {
   
-  // Fetch the fires-near-me data
-  let fires = await cached_fetch_json("https://prod.dataportal.rfs.nsw.gov.au/majorIncidents.json");
+  // Fetch NSW fires
+  let fires = (await cached_fetch_json("https://prod.dataportal.rfs.nsw.gov.au/majorIncidents.json", 15 * 60 * 1000)).features;
+
+  // Fetch VIV fires
+  fires.push(...(await cached_fetch_json("https://www.emergency.vic.gov.au/public/impact-areas-geojson.json", 15 * 60 * 1000)).features);
 
   // Async each fire to spread out weather requests
   let jobs = [];
-  for (let i=0; i<fires.features.length;i++) {
-    jobs.push(handle_fire(fires.features[i]));
+  for (let i=0; i<fires.length;i++) {
+    jobs.push(handle_fire(fires[i]));
   }
 
   // Wait for all the jobs to be completed
